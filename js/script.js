4b480abcdc65c424634ba480ce8efee135290547
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-06-09 22:02:15 +0200

    update

--- js/script.js
+++ js/script.js
@@ -42,32 +42,3 @@ function reveal() {
     }
   }
 }
-
-// showing menu when clicked on mobile version
-const showMenu = () => {
-  const menuBtn = document.querySelector(".menu-btn");
-  const closeMenuButton = document.querySelector(".close-menu");
-  const mainMenu = document.querySelector(".nav-links");
-
-  menuBtn.addEventListener("click", () => {
-    mainMenu.classList.toggle("show");
-  });
-
-  menuBtn.addEventListener("onkeydown", () => {
-    mainMenu.classList.toggle("show");
-  });
-};
-
-showMenu();
-
-//close menu
-const closeMenu = () => {
-  const closeMenuButton = document.querySelector(".close-menu");
-  const mainMenu = document.querySelector(".nav-links");
-
-  closeMenuButton.addEventListener("click", () => {
-    mainMenu.classList.remove("show");
-  });
-};
-
-closeMenu();
