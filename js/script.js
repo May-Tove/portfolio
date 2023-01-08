583f1e8dd43c3331509f568df31f48f09082f97d
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-01-08 21:08:46 +0100

    New updates

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 const faders = document.querySelectorAll(".fade-in");
-const sliders = document.querySelectorAll(".slide-in");
 
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -150px 0px",
-};
-
-const appearOnScroll = new IntersectionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-},
-appearOptions);
-
-sliders.forEach((slider) => {
-  appearOnScroll.observe(slider);
-});
-
+// fade in effect on project cards
 window.addEventListener("scroll", reveal);
 
 function reveal() {
